f216c420b0ea0681a417d64f56f061ef974a619a
Justin Herzog <justinherzog@justins-macbook-pro.local>
2018-09-06 09:39:00 -0600

    Debugged some things and added Meal.swift

---
+++ Food Tracker/Meal.swift
@@ -0,0 +1,34 @@
+//
+//  Meal.swift
+//  Food Tracker
+//
+//  Created by Justin Herzog on 9/4/18.
+//  Copyright © 2018 Justin Herzog. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    
+    //MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    //MARK: Initialization
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        
+        // Initialization should fail if there is no name or if the rating is negative.
+        if name.isEmpty || rating < 0 {
+            return nil
+        }
+        
+        // Initialize stored properties.
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+    }
+}
